ee309d93c564d3b86ec68617e5ed57e676ef93ae
MD.Shamiul Islam Shopnil <33134940+sishopnil@users.noreply.github.com>
2019-01-13 00:02:07 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include "includes/header.php"; ?>
+<?php include "includes/navigation.php"; ?>
+<div class="container-fluid text-center">
+    <h1>
+        Bcs prep
+    </h1>
+</div>
+
+
+<?php include "includes/footer.php"; ?>
